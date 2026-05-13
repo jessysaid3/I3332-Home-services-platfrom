e766183a27c973822b8d5287f9cc3fbc7cb35c95
CS-Studios <charbel.sayah@adontechnologies.com>
2026-05-13 22:52:33 +0300

    Update program.cs

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,3 +1,8 @@
+using HomeServicesPlatform.Data;
+using HomeServicesPlatform.Filters;
+using HomeServicesPlatform.Middleware;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddScoped<AuthFilter>();
 
 var app = builder.Build();
 
@@ -18,7 +38,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("AllowAll");
+app.UseMiddleware<AuthMiddleware>();
 
 app.MapControllers();
 
